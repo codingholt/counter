640fb0d2f1431185415182e84b4cbe2bac077ba5
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-08-30 12:04:29 +0200

    worked on a copy button but isnt functional yet

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ function countdown(){
     counter = counter - 1
     document.getElementById("number").innerHTML = counter + "."
 }
+
+function copy(){
+    let number = document.getElementById("number").innerHTML
+    navigator.clipboard.writeText(number)
+    alert('Copied ' + number)
+}
